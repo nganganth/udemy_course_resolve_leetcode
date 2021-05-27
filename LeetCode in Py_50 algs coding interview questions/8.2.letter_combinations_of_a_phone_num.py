1e8b40fdeae592b7a09cd7fb48eedfa731e9a45c
nganganth <huyennga154@gmail.com>
2021-05-27 12:09:30 +0900

    Update 8.2.letter_combinations_of_a_phone_num.py
    Complete Letter Combinations of Phone Number

--- LeetCode in Py_50 algs coding interview questions/8.2.letter_combinations_of_a_phone_num.py
+++ LeetCode in Py_50 algs coding interview questions/8.2.letter_combinations_of_a_phone_num.py
@@ -3,7 +3,17 @@
 
 class Solution:
     def backtracking(self, ans, m, digits, combination, index):
-
+        if index > len(digits):
+            return
+        if len(combination) == len(digits):
+            ans.append(combination[:])
+            return
+        
+        currDigit = digits[index]
+        currStr = m[currDigit]
+        
+        for i in range(len(currStr)):
+            self.backtracking(ans, m, digits, currStr[i], index + 1)
 
     def letterCombinations(self, digits: str) -> List[str]:
         ans = []
