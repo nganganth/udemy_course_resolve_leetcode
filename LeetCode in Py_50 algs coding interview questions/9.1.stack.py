b6363a955869bfc9e11f40a1434bebcfdee2b78b
nganganth <huyennga154@gmail.com>
2021-12-13 16:46:06 +0900

    Create 9.1.stack.py

---
+++ LeetCode in Py_50 algs coding interview questions/9.1.stack.py
@@ -0,0 +1,19 @@
+# Stack: A linear data structure which has the order of last in first out (LIFO)
+class PlateStack:
+  def __init__(self):
+    self.st = []
+  
+  def push(self, x):
+    self.st.append(x)
+  
+  def pop(self):
+    if len(self.st) > 0:
+      self.st.pop()
+  
+  def top(self):
+    if len(self.st) == 0:
+      return None
+    return self.st[-1]
+  
+  def getLen(self):
+    return len(self.st)
